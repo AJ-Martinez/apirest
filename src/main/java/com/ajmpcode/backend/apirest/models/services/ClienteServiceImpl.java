c923900ca292a34f274ca73d2d38d2e634473dac
AJ-Martinez <alejandrojmartinez04@outlook.com>
2021-02-17 19:23:03 -0300

    Primer commit

---
+++ src/main/java/com/ajmpcode/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ajmpcode.backend.apirest.models.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ajmpcode.backend.apirest.models.dao.IClienteDao;
+import com.ajmpcode.backend.apirest.models.entity.Cliente;
+
+@Service
+public class ClienteServiceImpl implements IClienteService{
+	
+	@Autowired
+	private IClienteDao clienteDao;
+
+	@Override
+	@Transactional
+	public List<Cliente> findAll() {
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Cliente findById(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		return clienteDao.save(cliente);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
+	}
+
+}
